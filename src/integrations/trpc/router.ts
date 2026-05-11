a80a2be852e7ace0f1baac7addecaefa72641865
Mario Lyon <mario@digileo.com>
2026-05-11 16:47:00 +0200

    feat: load data by fetching data from mock endpoint and appending to the database

--- src/integrations/trpc/router.ts
+++ src/integrations/trpc/router.ts
@@ -3,56 +3,24 @@ import {z} from "zod";
 import type {Measurement} from "#/types.ts";
 import {publicProcedure, router} from "./init";
 import {prisma} from "#/db.ts";
+import type {MeasurementModel} from "../../generated/prisma/models";
 
-let measurements: Measurement[] = [
-  {
-    id: 1,
-    client_id: "3837c9fc",
-    date_testing: "2019-10-02",
-    date_birthdate: "1950-01-01",
-    gender: 2,
-    ethnicity: 5,
-    creatine: 1.21,
-    chloride: 118.19,
-    fasting_glucose: 99.42,
-    potassium: 5.41,
-    sodium: 125.99,
-    total_calcium: 9.79,
-    total_protein: 7.9,
-    creatine_unit: "mgdl",
-    chloride_unit: "mmoll",
-    fasting_glucose_unit: "mgdl",
-    potassium_unit: "mmoll",
-    sodium_unit: "ul",
-    total_calcium_unit: "mgdl",
-    total_protein_unit: "gdl",
-  },
-  {
-    id: 2,
-    client_id: "3837c9fc",
-    date_testing: "2020-10-14",
-    date_birthdate: "1950-01-01",
-    gender: 2,
-    ethnicity: 5,
-    creatine: 0.39,
-    chloride: 90.58,
-    fasting_glucose: 137.9,
-    potassium: 1.77,
-    sodium: 68.86,
-    total_calcium: 13.18,
-    total_protein: 7.23,
-    creatine_unit: "mgdl",
-    chloride_unit: "mmoll",
-    fasting_glucose_unit: "mgdl",
-    potassium_unit: "mmoll",
-    sodium_unit: "ul",
-    total_calcium_unit: "mgdl",
-    total_protein_unit: "gdl",
-  },
-];
+async function fetchMoreMeasurements() {
+  const response = await fetch("https://mockapi-furw4tenlq-ez.a.run.app/data");
+  if (!response.ok) {
+    throw new Error(`Failed to fetch measurements: ${response.statusText}`);
+  }
+  const data = await response.json();
 
-function fetchMoreMeasurements() {
+  const formattedData = data.map((item: any) => ({
+    ...item,
+    date_testing: new Date(item.date_testing),
+    date_birthdate: new Date(item.date_birthdate),
+  }));
 
+  await prisma.measurement.createMany({
+    data: formattedData,
+  });
 }
 
 const measurementsRouter = {
@@ -61,44 +29,34 @@ const measurementsRouter = {
     .query(async (opts) => {
       const {input} = opts;
 
-      let result = []
-      while (result.length < 10){
-        result = await prisma.measurement.findMany({
-          skip: (input?.page || 1 - 1) * 10,
-          take: 10
+      let result: Measurement[] = [];
+      while (result.length < 10) {
+        const dbResults: MeasurementModel[] = await prisma.measurement.findMany({
+          skip: ((input?.page ?? 1) - 1) * 10,
+          take: 10,
         });
 
-        if(result.length < 10) {
-          fetchMoreMeasurements()
+        result = dbResults.map((r) => ({
+          ...r,
+          date_testing: r.date_testing.toISOString(),
+          date_birthdate: r.date_birthdate.toISOString(),
+          creatine: Number(r.creatine),
+          chloride: Number(r.chloride),
+          fasting_glucose: Number(r.fasting_glucose),
+          potassium: Number(r.potassium),
+          sodium: Number(r.sodium),
+          total_calcium: Number(r.total_calcium),
+          total_protein: Number(r.total_protein),
+        }));
+
+        if (result.length < 10) {
+          await fetchMoreMeasurements();
         }
       }
+      return result;
     }),
-  reset: publicProcedure.mutation(() => {
-    measurements = [
-      {
-        id: 3,
-        client_id: "ec8ab245",
-        date_testing: "2018-01-01",
-        date_birthdate: "1950-01-01",
-        gender: 1,
-        ethnicity: 5,
-        creatine: 0.62,
-        chloride: 104.66,
-        fasting_glucose: 83.72,
-        potassium: 2.78,
-        sodium: 82.36,
-        total_calcium: 5.81,
-        total_protein: 13.55,
-        creatine_unit: "mgdl",
-        chloride_unit: "mmoll",
-        fasting_glucose_unit: "mgdl",
-        potassium_unit: "mmoll",
-        sodium_unit: "ul",
-        total_calcium_unit: "mgdl",
-        total_protein_unit: "gdl",
-      },
-    ];
-    return measurements;
+  reset: publicProcedure.mutation(async () => {
+    await prisma.measurement.deleteMany()
   }),
 } satisfies TRPCRouterRecord;
 
